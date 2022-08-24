c3fc7a1c7290f3b7789a07583e76956904f41439
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-08-24 18:40:51 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+// multiply
+let num1 = 2;
+let num2 = 31;
+const multiply =  num1 * num2;
+
+// random
+let random = Math.floor(Math.random()*10);
+
+// mod
+let num3 = 39;
+let num4 = 7;
+let mod = num3 % num4;
+
+// max
+
+let max=Math.max(10, 15 , 20);
